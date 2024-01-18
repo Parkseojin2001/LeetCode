32619228895b60a2336270a9dc75d41ebb07d264
Parkseojin2001 <85439023+Parkseojin2001@users.noreply.github.com>
2024-01-18 19:35:29 +0900

    Time: 223 ms (5.07%), Space: 23.3 MB (6.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,10 +1,12 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-        # 정규식으로 불필요한 문자 필터링
-        s = re.sub('[^a-z0-9]', '', s)
-
-        return s == s[::-1]
-
-
-        
+        strs = []
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+        while len(strs) > 1:
+            if strs.pop(0) != strs.pop():
+                return False
+        return True
+            
+            
