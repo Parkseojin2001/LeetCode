ff0036c6e5e412267500dffb4699c27131896488
Parkseojin2001 <85439023+Parkseojin2001@users.noreply.github.com>
2025-02-14 01:55:24 +0900

    Time: 7 ms (32.74%), Space: 17.6 MB (93.8%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -4,22 +4,36 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        root = head = ListNode(0)
+    def reverse(self, head:ListNode):
+            node, prev = head, None
+            while node:
+                next, node.next = node.next, prev
+                prev, node = node, next
 
-        carry = 0
-        while l1 or l2 or carry:
-            sum = 0
-            # 두 입력값의 합 계산
-            if l1:
-                sum += l1.val
-                l1 = l1.next
-            if l2:
-                sum += l2.val
-                l2 = l2.next
-            # 몫(자리올림수)과 나머지(값) 계산
-            carry, val = divmod(sum + carry, 10)
-            head.next = ListNode(val)
-            head = head.next
+            return prev
+    def toList(self, node:ListNode):
+            list: List = []
+            while node:
+                list.append(node.val)
+                node = node.next
+            return list
+    def toReversedLinked(self, result:str):
+        prev = None
+        for r in result:
+            node = ListNode(int(r))
+            node.next = prev
+            prev = node
+        return node
+
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        a = self.toList(self.reverse(l1))
+        b = self.toList(self.reverse(l2))
+
+        result = int(''.join(str(e) for e in a)) + int(''.join(str(e) for e in b))
+
+        return self.toReversedLinked(str(result))
         
-        return root.next 
+        
+            
+            
+        
