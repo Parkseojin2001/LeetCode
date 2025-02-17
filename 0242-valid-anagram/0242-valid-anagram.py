7b72e1dd425d6712b931dece25de82f7d65904cf
Parkseojin2001 <85439023+Parkseojin2001@users.noreply.github.com>
2025-02-17 21:10:13 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t) and len(set(s)) != len(set(t)):
+            return False
+        hash_s = {}
+        hash_t = {}
+        for i in range(len(s)):
+            hash_s[s[i]] = 1 + hash_s.get(s[i], 0)
+            hash_t[t[i]] = 1 + hash_t.get(t[i], 0)
+        for i in range(len(s)):
+            if s[i] not in hash_t or hash_s[s[i]] != hash_t[s[i]]:
+                return False
+        return True
+        
