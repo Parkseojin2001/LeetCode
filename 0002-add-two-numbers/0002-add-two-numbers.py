25ba70bc5e999b19b67cad1c6d34a58fe88d08a0
Parkseojin2001 <85439023+Parkseojin2001@users.noreply.github.com>
2025-02-17 22:50:19 +0900

    Time: 4 ms (55.73%), Space: 17.9 MB (35.87%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -4,34 +4,23 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverse(self, head:ListNode):
-            node, prev = head, None
-            while node:
-                next, node.next = node.next, prev
-                prev, node = node, next
-
-            return prev
-    def toList(self, node:ListNode):
-            list: List = []
-            while node:
-                list.append(node.val)
-                node = node.next
-            return list
-    def toReversedLinked(self, result:str):
-        prev = None
-        for r in result:
-            node = ListNode(int(r))
-            node.next = prev
-            prev = node
-        return node
-
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        a = self.toList(self.reverse(l1))
-        b = self.toList(self.reverse(l2))
+        root = head = ListNode(0)
+        carry = 0
+        while l1 or l2 or carry:
+            sum = 0
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+            carry, val = divmod(sum + carry, 10)
+            head.next = ListNode(val)
+            head = head.next
+            
+        return root.next
 
-        result = int(''.join(str(e) for e in a)) + int(''.join(str(e) for e in b))
-
-        return self.toReversedLinked(str(result))
         
         
             
