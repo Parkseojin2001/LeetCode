01d082649cbc9f4403dda6926cea9fdc375ddc16
Parkseojin2001 <85439023+Parkseojin2001@users.noreply.github.com>
2025-02-23 23:06:56 +0900

    Time: 3 ms (68.11%), Space: 17.9 MB (47.88%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -1,10 +1,19 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        for char in sorted(set(s)):
-            suffix = s[s.index(char):] # 정렬된 set(s)의 첫번째 문자부터 시작
-            if set(s) == set(suffix):
-                return char + self.removeDuplicateLetters(suffix.replace(char, ''))
-        return ''
+        counter, seen, stack = collections.Counter(s), set(), []
+
+        for char in s:
+            counter[char] -= 1
+            if char in seen:
+                continue
+            # 스택에 문자 존재 & char이 스택에 가장 마지막에 들어간 문자보다 사전 순서가 빠름 & 스택에 가장 마지막에 들어간 문자가 1개이상으로 더 남음
+            while stack and char < stack[-1] and counter[stack[-1]] > 0:
+                seen.remove(stack.pop())
+
+            stack.append(char)
+            seen.add(char)
+
+        return ''.join(stack)
 
                 
 
