679c5939cc747a48f6a89318d5622263254073c4
Parkseojin2001 <85439023+Parkseojin2001@users.noreply.github.com>
2025-02-26 18:11:39 +0900

    Time: 0 ms (100%), Space: 17.9 MB (10.59%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,16 +1,9 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        hash_table = {}
-        for stone in stones:
-            if stone in hash_table:
-                hash_table[stone] += 1
-            else:
-                hash_table[stone] = 1 + hash_table.get(stone, 0)
-        
+        freqs = collections.Counter(stones)
         count = 0
-        
-        for jewel in jewels:
-            if jewel in hash_table:
-                count += hash_table[jewel]
+
+        for char in jewels:
+            count += freqs[char]
+
         return count
-        
