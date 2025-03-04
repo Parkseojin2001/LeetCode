3ee4b16dc3c835e5f65e658d185fef396c8221db
Parkseojin2001 <85439023+Parkseojin2001@users.noreply.github.com>
2025-03-04 23:26:51 +0900

    Time: 0 ms (100%), Space: 17.9 MB (31.03%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,16 +1,23 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        multi, num = 1, 0
-        for digit in digits[::-1]:
-            num = num + multi * digit
-            multi *= 10
-        
-        num += 1
-        L = list(str(num))
-        
-        for i in range(len(L)):
-            L[i] = int(L[i])
-        
-        return L
+        digits[-1] += 1
+        deq = collections.deque(digits)
+        dum = 0
+        for i in range(len(deq)-1, 0, -1):
+            deq[i] += dum
+            if deq[i] > 9:
+                dum = 1
+                deq[i] = deq[i] % 10
+            else:
+                dum = 0
+        if dum == 1:
+            deq[0] += 1
+            
+        if deq[0] > 9:
+            deq[0] = deq[0] % 10
+            deq.appendleft(1)
+
+        return list(deq)
+            
         
         
